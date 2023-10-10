ab695aa81b67fd26dd9bd1b15b77726c4292d270
Renildo Fagner <renildo.fagner@gmail.com>
2023-10-09 22:28:04 -0300

    Desafio finalizado!

--- Models/Tarefa.cs
+++ Models/Tarefa.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,7 +8,10 @@ namespace TrilhaApiDesafio.Models
 {
     public class Tarefa
     {
+        [Key]
         public int Id { get; set; }
+        
+        [Required(ErrorMessage = "O campo titulo é obrigatório")]
         public string Titulo { get; set; }
         public string Descricao { get; set; }
         public DateTime Data { get; set; }
